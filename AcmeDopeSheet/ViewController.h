e10a1838c7413bcc6617c569b0d9d2f3bf660ffd
andy Hazlett <andyhaz1970@gmail.com>
2016-03-22 16:17:24 -0700

    Initial Commit

---
+++ AcmeDopeSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcmeDopeSheet
+//
+//  Created by andrew hazlett on 3/22/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ AcmeDopeSheet/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AcmeDopeSheet
+//
+//  Created by andrew hazlett on 3/22/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
