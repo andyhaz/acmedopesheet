03ccbf3cd38bdaf6c1894480740414b6a24892eb
andy Hazlett <andyhaz1970@gmail.com>
2016-03-22 16:24:02 -0700

    add ADSheet (Acme Dope Sheet)

---
+++ ADSheet.h
@@ -0,0 +1,13 @@
+//
+//  ADSheet.h
+//  AcmeDopeSheet
+//
+//  Created by andrew hazlett on 3/22/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADSheet : NSObject
+
+@end
---
+++ ADSheet.m
@@ -0,0 +1,13 @@
+//
+//  ADSheet.m
+//  AcmeDopeSheet
+//
+//  Created by andrew hazlett on 3/22/16.
+//  Copyright © 2016 andrew hazlett. All rights reserved.
+//
+
+#import "ADSheet.h"
+
+@implementation ADSheet
+
+@end
